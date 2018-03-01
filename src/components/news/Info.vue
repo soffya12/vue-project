3d1e6af17e51a91d0815f27a82a37214cc5a653b
soffya <soffya@qq.com>
2018-03-01 21:44:39 +0800

    07-新闻详情页面完成

--- src/components/news/Info.vue
+++ src/components/news/Info.vue
@@ -1,15 +1,63 @@
 <template>
   <div>
-    这是新闻详情
+    <div class="news-info">
+      <header>
+        <h3 v-text="info.title"></h3>
+        <p class="news-desc">
+          <span>发表时间: {{ info.add_time | date }}</span>
+          <span>点击次数:  {{ info.click }}次</span>
+        </p>
+      </header>
+
+      <div class="news-content" v-html="info.content"></div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  created() {
+    this.getNewsInfoById()
+  },
+  data() {
+    return {
+      info: {
 
+      }
+    }
+  },
+  methods: {
+    getNewsInfoById() {
+      this.$http.get(`http://vue.studyit.io/api/getnew/${this.$route.params.id}`)
+      .then(res => {
+        const data = res.data
+        if (data.status === 0) {
+          console.log(data.message[0]);
+          this.info = data.message[0]
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
+  .news-info {
+    padding: 5px;
+    header {
+      border-bottom: 1px solid #ccc;
+    }
+    h3 {
+      font-size: 18px;
+    } 
+    .news-desc {
+      display: flex;
+      justify-content: space-between;
+      color: #26a2ff;
+    }
 
+    img {
+      width: 100%;
+    }
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,32 +2,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './assets/css/index.css'
-import VueRouter from 'vue-router'
 
 import './lib/mui/css/mui.css'
 import './lib/mui/css/icons-extra.css'
-Vue.use(VueRouter)
 
-import Home from './components/home/Home.vue'
-import Vip from './components/vip/Vip.vue'
-import Cart from './components/cart/Cart.vue'
-import Search from './components/search/Search.vue'
-import NewsList from './components/news/List.vue'
-import NewsInfo from './components/news/Info.vue'
 
-const router = new VueRouter({
-  routes: [
-    {path: '/', redirect: '/home'},
-    {path: '/home', component: Home },
-    {path: '/vip', component: Vip},
-    {path: '/cart', component: Cart},
-    {path: '/search', component: Search},
-    {path: '/home/newslist', component: NewsList}
 
-  ],
-  linkActiveClass: 'mui-active'
 
-})
 
 import { Header,Button,Swipe, SwipeItem  } from 'mint-ui';
 Vue.component(Header.name, Header);
@@ -38,10 +19,8 @@ Vue.component(SwipeItem.name, SwipeItem);
 import axios from 'axios'
 Vue.prototype.$http = axios;
 
-import moment from 'moment'
-Vue.filter('date', function(value, format = 'YYYY--MM-DD HH:mm:ss') {
-  return moment(value).format(format)
-})
+import './filters'
+import router from './router'
 
 new Vue({
   el: '#app',
