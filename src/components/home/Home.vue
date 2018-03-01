394347ec50cfd8fd57a7cdf880dcc7e12ce723b6
soffya <soffya@qq.com>
2018-03-01 19:23:38 +0800

    05-九宫格实现

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -6,33 +6,74 @@
         <img :src="item.img" :alt="item.url">
       </mt-swipe-item>
     </mt-swipe>
+
+    <!-- 九宫格 -->
+    <ul class="mui-table-view mui-grid-view mui-grid-9">
+      <li class="mui-table-view-cell mui-media mui-col-sm-4">
+        <a href="#">
+          <span class="mui-icon mui-menu-icon-1"></span>
+          <div class="mui-media-body">新闻资讯</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-sm-4">
+        <a href="#">
+          <span class="mui-icon mui-menu-icon-2"></span>
+          <div class="mui-media-body">图片分享</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-sm-4">
+        <a href="#">
+          <span class="mui-icon mui-menu-icon-3"></span>
+          <div class="mui-media-body">商品购买</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-sm-4">
+        <a href="#">
+          <span class="mui-icon mui-menu-icon-4"></span>
+          <div class="mui-media-body">留言反馈</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-sm-4">
+        <a href="#">
+          <span class="mui-icon mui-menu-icon-5"></span>
+          <div class="mui-media-body">视频专区</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-sm-4">
+        <a href="#">
+          <span class="mui-icon mui-menu-icon-6"></span>
+          <div class="mui-media-body">联系我们</div>
+        </a>
+      </li>
+
+    </ul>
   </div>
 </template>
 
 <script>
-// http://vue.studyit.io
-export default {
-  created() {
-    this.getLunbo();
-  },
-  data() {
-    return {
-      list: []
-    }
-  },
+  // http://vue.studyit.io
+  export default {
+    created() {
+      this.getLunbo();
+    },
+    data() {
+      return {
+        list: []
+      }
+    },
 
-  methods: {
-    getLunbo() {
-      this.$http.get('http://vue.studyit.io/api/getlunbo')
-      .then(res => {
-        const data = res.data
-        if (data.status === 0) {
-          this.list = data.message
-        }
-      })
+    methods: {
+      getLunbo() {
+        this.$http.get('http://vue.studyit.io/api/getlunbo')
+          .then(res => {
+            const data = res.data
+            if (data.status === 0) {
+              this.list = data.message
+            }
+          })
+      }
     }
   }
-}
 </script>
 
 <style lang="scss">
@@ -43,5 +84,34 @@ export default {
       width: 100%;
     }
   }
+  .mui-grid-view.mui-grid-9 {
+    background-color: #fff;
+    .mui-table-view-cell {
+    border: 0
+    }
 
+    .mui-icon {
+      width: 60px;
+      height: 60px;
+      background-size: contain;
+    }
+    .mui-menu-icon-1 {
+      background-image: url(../../assets/images/menu1.png);
+    }
+    .mui-menu-icon-2 {
+      background-image: url(../../assets/images/menu2.png);
+    }
+    .mui-menu-icon-3 {
+      background-image: url(../../assets/images/menu3.png);
+    }
+    .mui-menu-icon-4 {
+      background-image: url(../../assets/images/menu4.png);
+    }
+    .mui-menu-icon-5 {
+      background-image: url(../../assets/images/menu5.png);
+    }
+    .mui-menu-icon-6 {
+      background-image: url(../../assets/images/menu6.png);
+    }
+  }
 </style>
